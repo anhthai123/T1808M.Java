79f22c0d67874368062e8480045a450aa0348ec2
anhthai123 <44283631+anhthai123@users.noreply.github.com>
2019-05-06 10:25:11 +0700

    Add files via upload

---
+++ baithi2/KevinInterface.java
@@ -0,0 +1,10 @@
+package baithi2;
+
+import java.util.ArrayList;
+
+public interface KevinInterface<E>  {
+    ArrayList<E> getList();
+    boolean add(E e);
+    // boolean update(E e);
+    // boolean delete(E e);
+}
